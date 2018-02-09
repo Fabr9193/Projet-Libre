6ea0d2d9f3f72d2d26a4847afdf06845b2eab4b0
fabricio <fabricio.alegriaherrera@epitech.eu>
2018-02-09 09:33:34 +0100

    added js for db connection

---
+++ api/dbjs.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var mysql = require('mysql');
+var app = express();
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'admin'
+});
+
+connection.query('USE auteurs');
+
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+    connection.query('SELECT * FROM user as result', function(err, rows){
+        console.log('user', rows[0]);
+    });
+});
+
+app.listen(app.get('port'));
+console.log('Express server listening on port ' + app.get('port'));
